0a4d8f50045537b103797d312dc6e9ce33d34f53
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-27 11:09:04 -0700

    header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search'
+import Badge from '@mui/material/Badge'
+
+const Container = styled.div`
+  height: 60px;
+`
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+const Input = styled.input`
+  border: none;
+`
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`
+const Logo = styled.h1`
+  font-weight: bold;
+`
+const Right = styled.div`
+  flex: 1;
+`
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>English</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>MAMAZON</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary"></Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
