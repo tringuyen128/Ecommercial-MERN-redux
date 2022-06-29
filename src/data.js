d6b895043ac9686d34585aa347923943445b4fbf
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-29 14:44:24 -0700

    adding categories and image

--- src/data.js
+++ src/data.js
@@ -1,23 +1,44 @@
 export const sliderItems = [
   {
     id: 1,
-    img: './assets/home-image.png',
+    img: require('./assets/home-image.png'),
     title: 'CELEBRATE SUMMER',
     desc: 'Find all their favorites, and all the fun, in one place.',
-    bg: 'f5fafd',
+    bg: '#fcf1ed',
   },
   {
     id: 2,
-    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    img: require('./assets/slide2.png'),
     title: 'FEATURED SHOP',
     desc: "Search for popular styles and match your loves' styles.",
-    bg: 'fcf1ed',
+    bg: '#fcf1ed',
   },
   {
     id: 3,
-    img: 'https://i.ibb.co/cXFnLLV/3.png',
+    img: require('./assets/slide3.png'),
     title: 'NEW ARRIVALS',
     desc: 'New outfits to love for your little ones.',
-    bg: 'fbf0f4',
+    bg: '#fbf0f4',
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1533512930330-4ac257c86793?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bGl0dGxlJTIwYm95JTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+    title: 'BOYS FASHION',
+    cat: 'boys',
+  },
+  {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1629120578156-6b56dc438eea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGl0dGxlJTIwZ2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+    title: 'GIRLS FASHION',
+    cat: 'girls',
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1616181901157-3e403a88884d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3VtbWVyJTIwa2lkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+    title: 'STAY COOL!',
+    cat: 'summer',
   },
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Categories from '../Categories'
 import Annoucement from '../components/Annoucement'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
@@ -9,6 +10,7 @@ const Home = () => {
       <Annoucement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   )
 }
