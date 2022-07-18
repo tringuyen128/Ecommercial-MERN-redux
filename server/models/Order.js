21c8feb205f27b233aa116c0b02327a0bddceaf9
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-07-18 10:07:52 -0700

    Order model

---
+++ server/models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { tyoe: String, require: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, reuqired: true },
+    status: { type: String, default: 'pending' },
+  },
+  //will created at and updated at
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('User', OrderSchema)
